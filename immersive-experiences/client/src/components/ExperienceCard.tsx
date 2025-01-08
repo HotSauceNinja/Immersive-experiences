3ae4a3656710282d05c8fb532ba46f113af2ee67
HotSauceNinja <hotsauceninja@Sandras-MacBook-Pro.local>
2025-01-08 14:23:22 +0000

    Implement useState to show description details, add react-icons

--- immersive-experiences/client/src/components/ExperienceCard.tsx
+++ immersive-experiences/client/src/components/ExperienceCard.tsx
@@ -1,16 +1,29 @@
+import { useState } from "react";
+import { FaCalendarAlt, FaMapPin } from "react-icons/fa";
 import { Experience } from "../types";
 
 const ExperienceCard = ({ experience }: { experience: Experience }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  let description = showDetails
+    ? experience.description
+    : experience.description.slice(0, 100) + "...";
+
   return (
     <li key={experience._id}>
       <h2>{experience.title}</h2>
-      <p>{experience.description}</p>
+      <p>{description}</p>
+      <button onClick={() => setShowDetails((showDetails) => !showDetails)}>
+        {showDetails ? "Show less" : "Show more"}
+      </button>
       <p>
-        <strong>Location:</strong>
+        <FaMapPin />
+        <strong> Location: </strong>
         {experience.location || "No location provided"}
       </p>
       <p>
-        <strong>Date:</strong>
+        <FaCalendarAlt />
+        <strong> Date: </strong>
         {typeof experience.date === "string"
           ? experience.date
           : new Date(experience.date).toLocaleDateString()}
