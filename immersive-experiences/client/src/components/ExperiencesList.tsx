c2989590bf22cd112fc387c75183c107b69d8fcb
HotSauceNinja <hotsauceninja@Sandras-MacBook-Pro.local>
2025-01-09 14:06:43 +0000

    Add error state and refactor

--- immersive-experiences/client/src/components/ExperiencesList.tsx
+++ immersive-experiences/client/src/components/ExperiencesList.tsx
@@ -1,19 +1,31 @@
 // src/ExperiencesList.js
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { FaExclamationCircle, FaSpinner } from "react-icons/fa";
 import { Experience } from "../types";
 import ExperienceCard from "./ExperienceCard";
 
 const ExperiencesList = () => {
   const [experiences, setExperiences] = useState<Experience[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchExperiences = async () => {
       setLoading(true);
-      const response = await axios.get("http://localhost:5000/experiences");
-      setExperiences(response.data);
-      setLoading(false);
+      setError(null);
+      try {
+        const response = await axios.get("http://localhost:5000/experiences");
+        setExperiences(response.data);
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          setError(error.message);
+        } else {
+          setError("An unexpected error occurred");
+        }
+      } finally {
+        setLoading(false);
+      }
     };
     fetchExperiences();
   }, []);
@@ -21,16 +33,27 @@ const ExperiencesList = () => {
   return (
     <div>
       <h1>Immersive Experiences List</h1>
-      {loading && <p>Loading experiences...</p>}
-      <ul>
-        {experiences ? (
-          experiences.map((experience) => (
+      {loading ? (
+        <h3>
+          <FaSpinner /> Loading experiences...
+        </h3>
+      ) : error ? (
+        <div>
+          <h3>
+            <FaExclamationCircle /> Unable to load experiences. Please try again
+            later.
+          </h3>
+          <p>Error: {error}</p>
+        </div>
+      ) : experiences.length > 0 ? (
+        <ul>
+          {experiences.map((experience) => (
             <ExperienceCard key={experience._id} experience={experience} />
-          ))
-        ) : (
-          <li>No experiences found</li>
-        )}
-      </ul>
+          ))}
+        </ul>
+      ) : (
+        <h3>No experiences found</h3>
+      )}
     </div>
   );
 };
